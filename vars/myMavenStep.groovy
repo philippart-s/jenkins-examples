237a9aecf65002fa2b09e2ba0cb6a11b6a93f117
stephane <stephane.philippart@harmonie-mutuelle.fr>
2020-03-26 15:06:12 +0100

    Add params

--- vars/myMavenStep.groovy
+++ vars/myMavenStep.groovy
@@ -4,14 +4,14 @@ import fr.ourson.utils.Utilities
  * Custom step example.
  * @param args The maven args
  */
-def call(String args) {
+def call(Map config) {
 
     Utilities util = new Utilities(this)
 
     node() {
         stage('Build') {
             echo 'Building..'
-            myMaven 'compile'
+            myMaven config.mvnArgs
         }
         stage('Test') {
             echo 'Testing..'
